e26ba4ef6611db6e9234fc1962b12d1404406bb2
kazuaki matsumoto <81081558+ma2kaz@users.noreply.github.com>
2025-04-07 23:23:02 +0900

    Add files via upload

---
+++ cyber40.c
@@ -0,0 +1,11 @@
+
+
+
+const uint8_t matrix_rows = 4;
+const uint8_t matrix_cols = 12;
+
+const pin_t row_pins[] = { B0, B1, B2, B3 };
+const pin_t col_pins[] = { A0, A1, A2, A3, A4, A5, A6, A7, B4, B5, B6, B7 };
+
+
+
